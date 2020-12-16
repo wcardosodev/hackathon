20a7756f6148bd0ea9512b79ebed0f118d569141
Jordan Dey <jordandey1992@icloud.com>
2020-12-16 18:39:04 +0000

    merge conflict

---
+++ hackathon/src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Header = () => {
+
+    return(
+        <>
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="middle" href="https://statsbomb.com">
+                    <img src="https://statsbomb.com/wp-content/themes/statsbomb-2019/images/statsbomb-logo-white.svg" alt="Statsbomb" width="224" height="56" />
+                </a>
+            </div>
+        </nav>
+
+        <header className="App-header">
+        <h1 className="title margin-top has-text-centered">#Team Fuzz Hackathon Stuff :D</h1>
+            </header>
+        </>
+    )
+}
+
+export default Header;
