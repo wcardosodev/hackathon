5d23b7c4383301054088e33055d350b2db831be1
William Cardoso <wcardosodev@hotmail.com>
2020-12-17 10:25:26 +0000

    Classname

--- hackathon/src/Header.js
+++ hackathon/src/Header.js
@@ -1,22 +1,28 @@
 import React from 'react';
 
 const Header = () => {
+  return (
+    <>
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="middle" href="https://statsbomb.com">
+            <img
+              src="https://statsbomb.com/wp-content/themes/statsbomb-2019/images/statsbomb-logo-white.svg"
+              alt="Statsbomb"
+              width="224"
+              height="56"
+            />
+          </a>
+        </div>
+      </nav>
 
-    return(
-        <>
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-            <div class="navbar-brand">
-                <a class="middle" href="https://statsbomb.com">
-                    <img src="https://statsbomb.com/wp-content/themes/statsbomb-2019/images/statsbomb-logo-white.svg" alt="Statsbomb" width="224" height="56" />
-                </a>
-            </div>
-        </nav>
-
-        <header className="App-header">
-        <h1 className="title margin-top-lg has-text-centered">#Team Fuzz Hackathon Stuff :D</h1>
-            </header>
-        </>
-    )
-}
+      <header className="App-header">
+        <h1 className="title margin-top-lg has-text-centered">
+          #Team Fuzz Hackathon Stuff :D
+        </h1>
+      </header>
+    </>
+  );
+};
 
 export default Header;
